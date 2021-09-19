6c5eb8f1371adb1a63c5e1f49f690e0eb5082598
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-09-18 22:49:05 -0600

    initial commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctor appt',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Dentist appt',
+    day: 'May 8th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'clean house',
+    day: 'May 12th at 4:00pm',
+    reminder: true,
+  },
+];
